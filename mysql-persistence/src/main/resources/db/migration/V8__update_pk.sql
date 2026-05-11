faa8b8f32fe5cef1807d558c3ccdff9fd643f13c
bartek <pony830906@gmail.com>
2026-05-11 08:50:30 +0800

    sql migrate: workflow_created_on_index

--- mysql-persistence/src/main/resources/db/migration/V8__update_pk.sql
+++ mysql-persistence/src/main/resources/db/migration/V8__update_pk.sql
@@ -101,3 +101,5 @@ CALL FixPkIfNeeded('meta_workflow_def', 'name, version');
 
 CALL DropIndexIfExists('meta_task_def', 'unique_task_def_name');
 CALL FixPkIfNeeded('meta_task_def','name');
+
+ALTER TABLE workflow ADD INDEX `workflow_created_on_index` (created_on), ALGORITHM=INPLACE, LOCK=NONE;
